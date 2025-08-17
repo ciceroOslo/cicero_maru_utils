b66df105e1ac540b2c0386ec01350245506d611a
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2025-08-17 17:14:03 +0200

    Interim commit of parquet-to-lokfram script

--- src/cicero_maru_utils/scripts/maru_parquet_to_lokfram_variables.py
+++ src/cicero_maru_utils/scripts/maru_parquet_to_lokfram_variables.py
@@ -10,15 +10,42 @@ import argparse
 import dataclasses
 from pathlib import Path
 import typing as tp
+import sys
 
 import polars as pl
 import polars.selectors as cs
 import xlsxwriter
+from xlsxwriter import Workbook as XlsxWorkbook
 
 from cicero_maru_utils.labels.columns import (
     MaruCol,
     get_maru_cols
 )
+
+
+
+OutputObj: tp.TypeAlias = XlsxWorkbook
+InputObj: tp.TypeAlias = pl.LazyFrame
+
+def get_input_data_obj(in_file: Path|tp.IO[bytes]) -> InputObj:
+    """Get an input data object for the given input file object."""
+    if isinstance(in_file, Path) and not in_file.exists():
+        raise FileNotFoundError(f'Input file "{in_file}" does not exist.')
+    return pl.scan_parquet(in_file)
+
+def get_output_obj(out_file: Path|tp.IO[bytes]) -> OutputObj:
+    """Get an output data object for the given output file object."""
+    if isinstance(out_file, Path) and out_file.exists():
+        raise FileExistsError(f'Output file "{out_file}" already exists.')
+    return xlsxwriter.Workbook(out_file)
+
+
+def process_and_write_output(
+        input_obj: InputObj,
+        output_obj: OutputObj,
+        var_spec: OutputVarSpec,
+) -> None:
+    """Process input data into a single variable and write to output object."""
 
 
 
@@ -28,8 +55,8 @@ def main() -> None:
     """
     parser = argparse.ArgumentParser(
         description=(
-            "Create MarU variables for use in LOKFRAM from a parquet file of "
-            "processed data from MarU Excel reports."
+            'Create MarU variables for use in LOKFRAM from a parquet file of '
+            'processed data from MarU Excel reports.'
         ),
         epilog=(
             'The following output variable IDs are currently provided in the '
@@ -37,19 +64,49 @@ def main() -> None:
         ),
     )
     parser.add_argument(
-        "--in-file",
+        '--in-file',
         type=Path,
         required=True,
-        help="Path for the input .parquet file.",
+        help='Path for the input .parquet file.',
     )
     parser.add_argument(
-        "--out-file",
+        '--out-file',
         type=Path,
         required=True,
         help=(
-            "Path for the output file. Only excel output is supported at the "
-            "moment."
+            'Path for the output file. Only excel output is supported at the '
+            'moment.'
         ),
     )
 
+    args: argparse.Namespace = parser.parse_args()
 
+    print(f'Opening input file "{args.in_file}"...')
+    in_file: Path = args.in_file
+    print(f'Obtaining output object for output file "{args.out_file}"...')
+    out_file: Path = args.out_file
+
+    input_obj: InputObj = get_input_data_obj(in_file)
+    output_obj: OutputObj = get_output_obj(out_file)
+
+    curr_var: str = '<no variables processed yet>'
+    try:
+        for _var_name, _var_spec in output_var_specs.items():
+            curr_var = _var_name
+            print(f'Processing variable "{_var_name}"...')
+            process_and_write_output(
+                input_obj=input_obj,
+                output_obj=output_obj,
+                var_spec=_var_spec,
+            )
+    except Exception as err:
+        sys.stderr.write(
+            f'An error occurred while processing the variable "{curr_var}". '
+            f'Further processing was aborted.'
+        )
+        raise
+    finally:
+        print('Closing output file...')
+        output_obj.close()
+
+    print('Processing finished.')
