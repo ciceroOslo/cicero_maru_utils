f502286ec3e653fab1e514cb3a23741da9785624
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2025-08-17 18:36:21 +0200

    Interim commit, need to figure out where the output value column names should be specified.

--- src/cicero_maru_utils/processed_vars/specs.py
+++ src/cicero_maru_utils/processed_vars/specs.py
@@ -3,6 +3,7 @@
 This module contains specifications to use for output variables from processed
 MarU data, such as produced by the script `maru_xlsx_to_parquet.py`.
 """
+import enum
 import typing as tp
 
 import polars as pl
@@ -12,3 +13,43 @@ from cicero_maru_utils.labels.columns import MaruCol
 from .types import (
     OutputVarSpec,
 )
+
+
+
+class StavangerOutputVarNames(enum.StrEnum):
+    """Names of output variables for Stavanger."""
+    ENERGY_PER_PHASE_KWH = 'maru_energibehov_per_fase_kwh'
+
+group_by_common: tp.Final[tp.Sequence[str]] = (
+    MaruCol.municipality_name,
+    MaruCol.municipality_voyage_type,
+    MaruCol.year,
+)
+
+
+def _process_energy_per_phase_kwh(
+        df: pl.LazyFrame,
+        *,
+        maru_cols: MaruCol,
+        output_var_name: str,
+        output_value_col: str|None = None,
+) -> pl.LazyFrame:
+    """Process energy per phase in kWh."""
+    if output_value_col is None:
+        output_value_col = maru_cols.energy_kwh
+    return (
+        df
+        .group_by(*group_by_common, maru_cols.phase)
+        .agg(
+            pl.sum(maru_cols.energy_kwh).alias(output_value_col)
+        )
+    )
+
+
+stavanger_output_specs_202508: tp.Final[tp.Mapping[str, OutputVarSpec]] = {
+    StavangerOutputVarNames.ENERGY_PER_PHASE_KWH: OutputVarSpec(
+        name=StavangerOutputVarNames.ENERGY_PER_PHASE_KWH,
+        sheet_name=StavangerOutputVarNames.ENERGY_PER_PHASE_KWH,
+        processing_func=_process_energy_per_phase_kwh
+    )
+}
