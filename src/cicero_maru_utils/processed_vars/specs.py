a480efb11b67ef23d28a796bcf99d4d42f125b7d
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2025-08-23 23:32:44 +0200

    Added processing for maru_fuel_sum_tonn

--- src/cicero_maru_utils/processed_vars/specs.py
+++ src/cicero_maru_utils/processed_vars/specs.py
@@ -23,6 +23,7 @@ class StavangerOutputVarNames(enum.StrEnum):
     ENERGY_SUM_KWH = 'maru_energibehov_sum_kwh'
     ENERGY_PER_PHASE_KWH = 'maru_energibehov_per_fase_kwh'
     ENERGY_PER_GT_KWH = 'maru_energibehov_per_gt_kwh'
+    FUEL_SUM_TONN = 'maru_fuel_sum_tonn'
     FUEL_PER_GT_TONN = 'maru_fuel_per_gt_tonn'
 
 group_by_common: tp.Final[tp.Sequence[str]] = (
@@ -93,6 +94,26 @@ def _process_energy_per_gt_kwh(
     )
 
 
+def _process_fuel_sum_tonn(
+        df: pl.LazyFrame,
+        *,
+        maru_cols: MaruCol,
+        output_var_name: str,
+        output_value_col: str|None = None,
+) -> pl.LazyFrame:
+    """Process fuel sum in tonnes."""
+    if output_value_col is None:
+        output_value_col = maru_cols.fuel
+    return (
+        df
+        .group_by(*group_by_common)
+        .agg(
+            pl.sum(maru_cols.fuel).alias(output_value_col)
+        )
+        .sort(cs.exclude(cs.by_name(output_value_col)))
+    )
+
+
 def _process_fuel_per_gt_tonn(
         df: pl.LazyFrame,
         *,
@@ -129,6 +150,11 @@ stavanger_output_specs_202508: tp.Final[Mapping[str, OutputVarSpec]] = {
         name=StavangerOutputVarNames.ENERGY_PER_GT_KWH,
         sheet_name=StavangerOutputVarNames.ENERGY_PER_GT_KWH,
         processing_func=_process_energy_per_gt_kwh
+    ),
+    StavangerOutputVarNames.FUEL_SUM_TONN: OutputVarSpec(
+        name=StavangerOutputVarNames.FUEL_SUM_TONN,
+        sheet_name=StavangerOutputVarNames.FUEL_SUM_TONN,
+        processing_func=_process_fuel_sum_tonn
     ),
     StavangerOutputVarNames.FUEL_PER_GT_TONN: OutputVarSpec(
         name=StavangerOutputVarNames.FUEL_PER_GT_TONN,
